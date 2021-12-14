b0e2be99b0336cb153199b38f10f951dd54fad66
“VenuReddy773” <venu706@gmail.com>
2021-12-14 18:47:10 +0530

    [VenuGopalReddy] Added: Created Employee Table

--- EmployeePayRoll/EmployeePayRoll.sql
+++ EmployeePayRoll/EmployeePayRoll.sql
@@ -2,3 +2,13 @@
 create database payroll_service
 use payroll_service
 
+
+--UC2-Creating Table
+create table employee_payroll
+(   emp_id int  not null identity(1,1) primary key,
+	Emp_name varchar(50),
+	salary double precision not null ,
+	start date not null,
+)
+
+
